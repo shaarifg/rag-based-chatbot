60beda60cdbe5290c19f5754eee6e7d789046c25
Mohammad Sharif <ermohdsharif@gmail.com>
2025-11-26 01:48:02 +0530

    good deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/chatbot/", // Add this
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
